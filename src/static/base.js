b8521e57439efdac02aea33a074d84453aa8684d
Lauri <1960498+msha@users.noreply.github.com>
2022-01-11 23:32:25 +0200

    minor adjustements and additions

--- src/static/base.js
+++ src/static/base.js
@@ -1,46 +1,41 @@
 document.addEventListener("submit", sendData);
 document.addEventListener("update", getDudes);
-
 function sendData(e) {
     e.preventDefault();
-    const name = document.querySelector("#name").value;
+    var name = document.querySelector("#name").value;
     fetch("/create_dude", {
-            method: "POST",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                name: name,
-            })
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            name: name
         })
-        .then(res => res.json())
-        .then(data => {
-            const { result } = data;
-            document.querySelector("#dude").innerHTML = data.return;
-        })
-        .catch(err => console.log(err));
-};
-
-function getDudes(e) {
-    fetch("/get_dudes", {
-            method: "GET",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json"
-            }
-        })
-        .then(res => res.json())
-        .then(data => {
-            document.querySelector("#dude").innerHTML = ''
-            data.forEach(element => {
-                document.querySelector("#dude").innerHTML += element.character_name + '<br>';
-            });
-
-        })
-
+    })
+        .then(function (res) { return res.json(); })
+        .then(function (data) {
+        var result = data.result;
+        document.querySelector("#dude").innerHTML = data["return"];
+    })["catch"](function (err) { return console.log(err); });
 }
-
-setInterval(function() {
+;
+function getDudes() {
+    fetch("/get_dudes", {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+        }
+    })
+        .then(function (res) { return res.json(); })
+        .then(function (data) {
+        document.querySelector("#dude").innerHTML = '';
+        data.forEach(function (element) {
+            document.querySelector("#dude").innerHTML += element.character_name + '<br>';
+        });
+    });
+}
+setInterval(function () {
     getDudes();
 }, 500);
