2672c6486440bd6491ab716d3e85c48002f0be8c
Lauri <1960498+msha@users.noreply.github.com>
2022-01-17 23:48:57 +0200

    minor typescript fixes

--- src/static/base.ts
+++ src/static/base.ts
@@ -1,99 +1,21 @@
+'use strict';
 document.addEventListener("submit", sendData);
 document.addEventListener("update", getDudes);
 
-const socket = io.connect('localhost:5000');
+///<reference path="socket.io.js"/>
+const socket = io.connect('');
 
 const app = new PIXI.Application();
-document.body.appendChild(app.view);
-
-// create a new background sprite
-const background = PIXI.Sprite.from('examples/assets/bg_rotate.jpg');
-background.width = app.screen.width;
-background.height = app.screen.height;
-app.stage.addChild(background);
-
-// create an array to store a reference to the dudes
-const dudeArray = [];
-
-const totaldudes = 20;
-
-for (let i = 0; i < totaldudes; i++) {
-    // create a new Sprite that uses the image name that we just generated as its source
-    const dude = PIXI.Sprite.from('examples/assets/flowerTop.png');
-
-    dude.anchor.set(0.5);
-
-    // set a random scale for the dude
-    dude.scale.set(0.8 + Math.random() * 0.3);
-
-    // finally let's set the dude to be at a random position...
-    dude.x = Math.floor(Math.random() * app.screen.width);
-    dude.y = Math.floor(Math.random() * app.screen.height);
-
-    // The important bit of this example, this is how you change the default blend mode of the sprite
-    dude.blendMode = PIXI.BLEND_MODES.ADD;
-
-    // create some extra properties that will control movement
-    dude.direction = Math.random() * Math.PI * 2;
-
-    // this number will be used to modify the direction of the dude over time
-    dude.turningSpeed = Math.random() - 0.8;
-
-    // create a random speed for the dude between 0 - 2
-    dude.speed = 2 + Math.random() * 2;
-
-    // finally we push the dude into the dudeArray so it it can be easily accessed later
-    dudeArray.push(dude);
-
-    app.stage.addChild(dude);
-}
-
-// create a bounding box for the little dudes
-const dudeBoundsPadding = 100;
-
-const dudeBounds = new PIXI.Rectangle(
-    -dudeBoundsPadding,
-    -dudeBoundsPadding,
-    app.screen.width + dudeBoundsPadding * 2,
-    app.screen.height + dudeBoundsPadding * 2,
-);
-
-app.ticker.add(() => {
-    // iterate through the dudes and update the positions
-    for (let i = 0; i < dudeArray.length; i++) {
-        const dude = dudeArray[i];
-        dude.direction += dude.turningSpeed * 0.01;
-        dude.x += Math.sin(dude.direction) * dude.speed;
-        dude.y += Math.cos(dude.direction) * dude.speed;
-        dude.rotation = -dude.direction - Math.PI / 2;
-
-        // wrap the dudes by testing their bounds...
-        if (dude.x < dudeBounds.x) {
-            dude.x += dudeBounds.width;
-        } else if (dude.x > dudeBounds.x + dudeBounds.width) {
-            dude.x -= dudeBounds.width;
-        }
-
-        if (dude.y < dudeBounds.y) {
-            dude.y += dudeBounds.height;
-        } else if (dude.y > dudeBounds.y + dudeBounds.height) {
-            dude.y -= dudeBounds.height;
-        }
-    }
-});
-
-
-
 
 socket.on('connect',function() {
     socket.send('User connected')
 })
 
-socket.on('message',function(msg){
+socket.on('message',function(msg: typeof event){
     getDudes(msg);
 })
 
-function sendData(e) {
+function sendData(e: any) {
     e.preventDefault();
     const name = (<HTMLInputElement>document.querySelector("#name")).value;
     fetch("/create_dude", {
@@ -110,12 +32,16 @@ function sendData(e) {
         .catch(err => console.log(err));
 };
 
-function getDudes(dudes) {
+function getDudes(dudes: any) {
     let work = JSON.parse(dudes);
-            document.querySelector("#dude").innerHTML = ''
-            Object.entries(work).forEach(element => {
+    let dude = document.querySelector("#dude");
+    if (dude !== null) {
+        dude.innerHTML = ''
+            Object.entries(work).forEach((element: any) => {
                 let char = element[1];
-                document.querySelector("#dude").innerHTML += char.character_name +'       level: '+  char.character_level+ '<br>'; 
+                if(dude !== null) {
+                    dude.innerHTML += char.character_name +'       level: '+  char.character_level+ '<br>'; 
+                }
             });
-
+    }
 }
